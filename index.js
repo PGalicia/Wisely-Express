8667621e76ac22fe1f5926abcbad419f1617e3c1
Patrick Galicia <galicia.patrick@gmail.com>
2025-01-07 20:00:11 -0800

    Add error in mongoose connect

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 mongoose
   .connect(process.env.ATLAS_URI)
   .then(() => console.log("Connected to MongoDB..."))
-  .catch((err) => console.error("Could not connect to MongoDB..."));
+  .catch((err) => console.error("Could not connect to MongoDB...", err));
 
 app.use(cors({
   origin: process.env.CORS_ORIGIN
