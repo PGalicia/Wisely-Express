8c2caadcb103edd7a0e7f3762813837284d6c859
Patrick Galicia <galicia.patrick@gmail.com>
2025-01-07 23:02:03 -0800

    Test default

--- index.js
+++ index.js
@@ -28,6 +28,21 @@ console.log('process.env.ATLAS_URI', process.env.ATLAS_URI);
 // );
 
 // Ensure the app binds to all network interfaces
-app.listen(PORT, '127.0.0.1', () => {
-  console.log(`Server running on port ${PORT}`);
+// app.listen(PORT, '127.0.0.1', () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
+const http = require('node:http');
+
+const hostname = '127.0.0.1';
+const port = 8080;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Elastic Beanstalk!\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
